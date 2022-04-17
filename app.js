7dd262b559fc6f2e47bac0ba5c52a52a9e782032
FARIDJADDA <faridjadda750@gmail.com>
2022-04-17 04:08:22 +0200

    HTTP request

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require("http");
+const server = http.createServer();
 
-const server = http.createServer((req, res) => {
-  res.end();
+server.on("request", (req, res) => {
+  res.writeHead(200, {
+    "content-type": "text/html",
+  });
+
+  res.end(`"got a response"`);
 });
 
 server.listen(8080);
