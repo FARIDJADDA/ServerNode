95b4e0e6d49eb9ec532499fd7161bdd4874f9075
FARIDJADDA <faridjadda750@gmail.com>
2022-04-17 04:39:02 +0200

    http routing with fs readFileSync

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const fs = require("fs");
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  res.writeHead(200, {
-    "content-type": "text/html",
-  });
-  const fileContent = fs.readFileSync("./index.html", "utf8");
-  const template = fileContent.replace("{{name}}", "Fafa");
-  res.end(template);
+  const url = req.url;
+  let fileContent;
+  if (url === "/home" || "/") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    fileContent = fs.readFileSync("./index.html", "utf8");
+  } else if (url === "/info") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    fileContent = fs.readFileSync("./info.html", "utf8");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    });
+    fileContent = fs.readFileSync("./not-found.html", "utf8");
+  }
+
+  res.end(fileContent);
 });
 
 server.listen(8080);
